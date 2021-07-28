e3cd7f3963518a124ce320892e26a4ae2c9bda44
bryanyunis <bryan.yunis@gateless.com>
2021-07-27 21:47:22 -0700

    add user routes

--- src/simple_http/core.clj
+++ src/simple_http/core.clj
@@ -5,19 +5,34 @@
             [reitit.ring.middleware.muuntaja :as muuntaja]))
             
 
-(use 'ring.adapter.jetty)
-
+(def users (atom {}))
 (defn string-handler [_]
   {:status 200
    :body "hey cowboy"})
+
+(defn create-user [{user :body-params}]
+  (let [id (str (java.util.UUID/randomUUID))
+        users (->> (assoc user :id id)
+                   (swap! users assoc id))]
+    {:status 200
+     :body users}))
+ 
+(defn get-users [_]
+  {:status 200
+   :body @users})
+
+(defn get-user-by-id [{{:keys [id]} :path-params}]
+  {:status 200
+   :body (get @users id)})
 
 (def app
   (ring/ring-handler
     (ring/router
       ["/"
-       ["" {:get string-handler}]
-       ["/math"
-        {:post string-handler}]]
+       ["users/:id" get-user-by-id]
+       ["users" {:get get-users
+                 :post create-user}]
+       ["" string-handler]]
       {:data {:muuntaja m/instance
               :middleware [muuntaja/format-middleware]}})))
                            
